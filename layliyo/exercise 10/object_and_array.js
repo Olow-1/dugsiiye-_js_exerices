d114cbb16fc6a7555538894c9513171d1391b35e
Olow-1 <apdaladiirshe@gmail.com>
2026-07-04 12:49:11 +0300

    submitted exercise 10

---
+++ layliyo/exercise 10/object_and_array.js
@@ -0,0 +1,22 @@
+let libary =[
+
+{
+     tittle : "how to win freinds and influence peaople",
+     author : "dale carnegie",
+     year :  2020
+
+},
+
+{
+   
+    tittle :  "automatic hapit",
+    author : "unknown😆",
+    year : 2022
+
+}
+
+
+]
+
+console.log(libary[0].tittle);
+console.log(libary[1].author)
---
+++ layliyo/exercise 13/for_loob_aray.js
@@ -0,0 +1,8 @@
+const fruit = ["apple", "mangoo", "lemonon"]
+
+for (let i = 0; i<fruit.length;i++){
+
+    console.log(fruit[i])
+}
+
+console.log(fruit.length)
